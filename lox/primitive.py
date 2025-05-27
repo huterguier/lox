78ca801860b0fc274c60ebceb67325f7b339f247
huterguier <huterguier@gmail.com>
2025-05-27 12:01:36 +0200

    fixed batching

--- lox/primitive.py
+++ lox/primitive.py
@@ -9,9 +9,11 @@ from jax.interpreters.mlir import emit_python_callback
 lox_p = core.Primitive("lox")
 lox_p.multiple_results = True
 
-def log(data, step=None):
-    def callback(data):
-        print("Logging data:", data)
+def log(data, step=None, callback=None):
+    if callback is None:
+        def _callback(data):
+            print("Logging data:", data)
+        callback = _callback
     data_flat, structure = jax.tree.flatten(data)
     return lox_p.bind(*data_flat, structure=structure, callback=callback)
 
@@ -31,9 +33,10 @@ def lox_lowering(*data_flat, structure, callback):
     return data_flat
 mlir.register_lowering(lox_p, mlir.lower_fun(lox_lowering, multiple_results=True))
 
-def lox_batch(vector_arg_values, batch_axes):
-    outs = log(*vector_arg_values)
-    return outs, (0,) * len(outs)
+def lox_batch(vector_arg_values, batch_axes, structure, callback):
+    outs = lox_p.bind(*vector_arg_values, structure=structure, callback=callback)
+    print("batch axes:", batch_axes)
+    return outs, batch_axes
 batching.primitive_batchers[lox_p] = lox_batch
 
 def lox_jvp(arg_values, arg_tangents):
