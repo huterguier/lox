5f65a1962c23db3f83248cd7160c43cff80bbc47
huterguier <huterguier@gmail.com>
2025-06-14 11:34:54 +0200

    updated theme

--- docs/conf.py
+++ docs/conf.py
@@ -23,5 +23,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_logo = '_static/lox.png'
+
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
+html_css_files = [
+    'style.css',
+]
