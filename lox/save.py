9c4e9bd4056f0e655499e302bc56cd2b95fbd314
huterguier <huterguier@gmail.com>
2025-06-27 19:55:19 +0200

    reworked saving

--- lox/save.py
+++ lox/save.py
@@ -8,35 +8,67 @@ from typing import Any
 import lox
 
 
-def save(data: dict[str, Any], path: lox.String, key: jax.Array = None):
-  def _callback(data: dict[str, Any], path, key):
+def save(data: dict[str, Any], path: lox.String, mode: str='a', key: jax.Array = None):
+  """
+  Save data to a specified path using a callback function. Each entry in the data dictionary is saved as a separate file with the key as the filename.
+  Args:
+    data (dict[str, Any]): The data to be saved.
+    path (lox.String): The path where the data will be saved.
+    mode (str): The mode in which to open the file ('a' for append, 'w' for write, 'x' for exclusive creation).
+    key (jax.Array, optional): An optional key to differentiate data when saving.
+  """
+  def callback(data: dict[str, Any], path, mode, key):
+    if mode not in ['a', 'w', 'x']:
+      raise ValueError("Mode must be 'a', 'w', or 'x'.")
     path = str(lox.String(path))
     if key is not None:
       path = path + '/' + f"{key[0]}{key[1]}"
 
-    for key in data:
-      file = path + '/' + key + '.pkl'
-      if os.path.exists(file):
-        with open(file, 'rb') as f:
-          value = pickle.load(f)
-        value = jax.tree.map(lambda v, d: jnp.concatenate([v, d]), value, data[key])
+    if mode == 'a':
+      for key in data:
+        file = path + '/' + key + '.pkl'
+        if os.path.exists(file):
+          with open(file, 'rb') as f:
+            value = pickle.load(f)
+          value = jax.tree.map(
+            lambda v, d: jnp.concatenate([jnp.atleast_1d(v), jnp.atleast_1d(d)]),
+            value,
+            data[key]
+          )
+          with open(file, 'wb') as f:
+            pickle.dump(value, f)
+        else:
+          if not os.path.exists(path):
+            os.makedirs(path)
+          with open(file, 'wb') as f:
+            pickle.dump(data[key], f)
+
+    elif mode == 'w':
+      for key in data:
+        file = path + '/' + key + '.pkl'
+        if not os.path.exists(path):
+          os.makedirs(path)
         with open(file, 'wb') as f:
-          pickle.dump(value, f)
-      else:
+          pickle.dump(data[key], f)
+
+    elif mode == 'x':
+      if os.path.exists(path):
+        raise FileExistsError(f"Path {path} already exists.")
+      for key in data:
+        file = path + '/' + key + '.pkl'
         if not os.path.exists(path):
           os.makedirs(path)
         with open(file, 'wb') as f:
           pickle.dump(data[key], f)
 
   jax.debug.callback(
-    _callback,
+    callback,
     ordered=True,
     data=data,
     path=path.value,
+    mode=mode,
     key=key
   )
-
-  return
 
 
 # def load(path, result_shape_dtypes):
