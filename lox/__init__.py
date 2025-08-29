7c336dd0ca804bcc684e5dcce85d15afaa90f8f6
huterguier <huterguier@gmail.com>
2025-08-29 16:52:08 +0200

    added save logger

--- lox/__init__.py
+++ lox/__init__.py
@@ -3,17 +3,18 @@ from .tap import tap
 from .primitive import log
 from .util import String, string
 from .save import save
+from .save import SaveLogger
 from .logdict import logdict
 from .logger import Logger, LoggerState, MultiLogger
 import lox.wandb as wandb
 
 __all__ = [
-    'spool',
-    'tap',
-    'log',
-    'String',
-    'string',
-    'save',
-    'logdict',
-    'wandb',
+    "spool",
+    "tap",
+    "log",
+    "String",
+    "string",
+    "save",
+    "logdict",
+    "wandb",
 ]
