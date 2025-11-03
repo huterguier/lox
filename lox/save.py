0a6aea79573231792ec115edf32b475c7ee60cc6
huterguier <huterguier@gmail.com>
2025-11-03 20:24:59 +0100

    add load_callback

--- lox/save.py
+++ lox/save.py
@@ -1,18 +1,20 @@
 import os
 import pickle
-from typing import Any, Optional
+from typing import Any, Iterable, Optional
 
 import jax
+import jax.experimental
 import jax.numpy as jnp
 
 from lox.string import StringArray
+from lox.typing import Key
 
 
 def save_callback(
     data: dict[str, Any],
     path: StringArray | str,
     mode: str = "a",
-    key: Optional[jax.Array] = None,
+    key: Optional[Key] = None,
 ):
     if mode not in ["a", "w", "x"]:
         raise ValueError("Mode must be 'a', 'w', or 'x'.")
@@ -71,3 +73,45 @@ def save(
     jax.debug.callback(
         save_callback, ordered=True, data=data, path=path, mode=mode, key=key
     )
+
+
+def load_callback(
+    path: StringArray | str,
+    result_shape_dtypes: Any,
+    argnames: Iterable[str],
+    key: Optional[Key] = None,
+) -> dict[str, Any]:
+    def callback(key, path):
+        if key is not None:
+            key_data = jax.random.key_data(key)
+            folder_name = str(int(f"{key_data[0]}{key_data[1]}"))
+            path = path + "/" + folder_name
+        data = {}
+        for argname in argnames:
+            file_path = path + f"/{argname}.pkl"
+            with open(file_path, "rb") as f:
+                data[argname] = pickle.load(f)
+        return data
+
+    return jax.experimental.io_callback(
+        callback,
+        result_shape_dtypes,
+        path=path,
+        key=key,
+    )
+
+
+def load(
+    path: StringArray | str,
+    result_shape_dtypes: Any = None,
+    key: Optional[Key] = None,
+) -> dict[str, Any]:
+    """
+    Load data from a specified path. Each file in the directory is loaded into a dictionary with the filename (without extension) as the key.
+    Args:
+      path (lox.String): The path from which the data will be loaded.
+      key (jax.Array, optional): An optional key to differentiate data when loading.
+    Returns:
+        dict[str, Any]: The loaded data.
+    """
+    raise NotImplementedError("Load function is not implemented yet.")
