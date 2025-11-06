57bd71df52686c585b63a38664b415a82b3a2f8a
huterguier <huterguier@gmail.com>
2025-11-06 17:16:59 +0100

    Refactor commeon functions into file

--- tests/test_tap.py
+++ tests/test_tap.py
@@ -2,72 +2,10 @@ import contextlib
 import io
 
 import jax
-import jax.numpy as jnp
 import pytest
+from functions import *
 
 import lox
-
-
-def f_id(x):
-    lox.log({"x": x})
-    return x
-
-
-def f_add(x):
-    z = x + x
-    lox.log({"x": x, "z": z})
-    return z
-
-
-def f_scan(x):
-    def step(carry, x):
-        carry = carry + x.mean()
-        lox.log({"carry": carry, "x": x})
-        return carry, carry
-
-    return jax.lax.scan(step, 0, x)
-
-
-def f_call(x):
-    def g(x):
-        lox.log({"x": x})
-        return x * 2
-
-    return g(x) + 1
-
-
-def f_jit(x):
-    @jax.jit
-    def g(x):
-        lox.log({"x": x})
-        return x * 3
-
-    return g(x) + 1
-
-
-def f_cond(x):
-    def true_fun(x):
-        x = x + 1
-        lox.log({"branch": True, "x": x})
-        return x
-
-    def false_fun(x):
-        x = x - 1
-        lox.log({"branch": False, "x": x})
-        return x
-
-    cond = x.ravel()[0] > 0
-    return jax.lax.cond(cond, true_fun, false_fun, x)
-
-
-def f_grad(x):
-    def func(x):
-        lox.log({"x": x})
-        return x.mean()
-
-    grad_func = jax.grad(func)
-    return grad_func(x)
-
 
 functions = [
     (f_id),
